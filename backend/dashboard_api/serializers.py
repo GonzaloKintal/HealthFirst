3ea9d947f4c9148e2b44593d763391030fdaeff2
fabihunt <fabi.hunt93@gmail.com>
2025-04-29 19:36:15 -0300

    update endpoint users

--- backend/dashboard_api/serializers.py
+++ backend/dashboard_api/serializers.py
@@ -4,10 +4,11 @@ from .models import HealthFirstUser
 
 class HealthFirstUserSerializer(serializers.ModelSerializer):
     full_name = serializers.SerializerMethodField()
+    role = serializers.SlugRelatedField(read_only=True, slug_field='name')
 
     class Meta:
         model = HealthFirstUser
-        fields = ['id', 'full_name', 'user_type','email']
+        fields = ['id', 'full_name', 'role','email']
 
     def get_full_name(self, obj):
         return f"{obj.first_name} {obj.last_name}"
