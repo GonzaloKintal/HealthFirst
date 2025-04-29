3fae5ac367337960eaddb8a9421ea5aaf1e0b941
kalutwarro1 <kalutwarro@gmail.com>
2025-04-29 19:51:14 -0300

    feat: adds licenses list endpoint

--- backend/dashboard_api/serializers.py
+++ backend/dashboard_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import HealthFirstUser
+from .models import HealthFirstUser, License
 
 
 class HealthFirstUserSerializer(serializers.ModelSerializer):
@@ -11,3 +11,26 @@ class HealthFirstUserSerializer(serializers.ModelSerializer):
 
     def get_full_name(self, obj):
         return f"{obj.first_name} {obj.last_name}"
+    
+
+class LicenseSerializer(serializers.ModelSerializer):
+    employee = serializers.CharField(source='user.get_full_name')
+    days = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+
+    class Meta:
+        model = License
+        fields = ['license_id', 'employee', 'type', 'start_date', 'end_date', 'days', 'status']
+
+    def get_days(self, obj):
+        return (obj.end_date - obj.start_date).days + 1
+
+    def get_status(self, obj):
+        if obj.justified:
+            return 'Approved'
+        elif obj.closing_date:
+            return 'Rejected'
+        else:
+            return 'Pending'    
+
+
