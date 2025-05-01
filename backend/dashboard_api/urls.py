f90db80bab30f83dcf2f64a9bda1461d8b9a7b37
kalutwarro1 <kalutwarro@gmail.com>
2025-05-01 14:19:56 -0300

    feat: adds licenses requests endpoint

--- backend/dashboard_api/urls.py
+++ backend/dashboard_api/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('users/update/<int:id>', update_user, name='update_user'),
 
     path('licenses', licenses_list,name='licenses_list'),
+    path('licenses/request', create_license, name='create_license'),
+
      
 ]
