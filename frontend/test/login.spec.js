637664af1d27eeed8ce4317c03c56478db2480be
Juan Saltiva <66804342+juansaltiva2000@users.noreply.github.com>
2025-05-05 13:10:39 -0300

    test licencias

--- frontend/test/login.spec.js
+++ frontend/test/login.spec.js
@@ -1,11 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('Login exitoso con credenciales correctas', async ({ page }) => {
+test('Login exitoso del usuario cliente', async ({ page }) => {
   await page.goto('http://localhost:5173/login');
-  await page.fill('#username', 'cliente@gmail.com');  // Ejemplo de credenciales válidas
+
+  // Ingresar credenciales válidas
+  await page.fill('#username', 'cliente@gmail.com');
   await page.fill('#password', '1234');
   await page.click('button[type="submit"]');
 
-  // Verificar que la URL cambió correctamente después de hacer login
-  await expect(page).toHaveURL('http://localhost:5173/employee');  // Cambiar a la URL correcta
+  await expect(page).toHaveURL('http://localhost:5173/employee', { timeout: 10000 });
+
+
+
 });
--- frontend/test/pedir_licencia.spec.js
+++ frontend/test/pedir_licencia.spec.js
@@ -1,7 +1,31 @@
-/////// debemos loguiarlo
-/////// ir  a la pagina de pedir licencia
-/////// llenar el formulario de pedir licencia  
-/////// enviar el formulario
-/////// verificar que se haya enviado correctamente
-//
-import { test, expect } from '@playwright/test';   
+// test/licencia.spec.js
+import { test, expect } from '@playwright/test';
+
+test('Usuario solicita una licencia correctamente', async ({ page }) => {
+  // Paso 1: Loguearse usando la función login importada
+  await page.goto('http://localhost:5173/login');
+
+  // Ingresar credenciales válidas
+  await page.fill('#username', 'cliente@gmail.com');
+  await page.fill('#password', '1234');
+  await page.click('button[type="submit"]');
+
+  await expect(page).toHaveURL('http://localhost:5173/employee', { timeout: 10000 });
+
+
+  // Paso 2: Navegar a la página de solicitud de licencia
+  await page.click('span.ml-3.font-medium:text("Solicitar Licencia")');
+
+
+  // Paso 3: Completar los campos del formulario
+  await page.selectOption('select[name="licenseType"]', { value: 'Enfermedad' });
+  await page.fill('[name="startDate"]', '2025-05-10');
+  await page.fill('[name="endDate"]', '2025-05-11');
+  await page.type('textarea[name="reason"]', 'Gripe con fiebre alta');  // Simula que el usuario escribe
+  await page.check('input[name="declaration"]');
+
+
+  // Paso 4: Enviar el formulario
+  await page.click('button[type="submit"]');
+
+});
