b93612fd29d40a5477e2502228add535c94f307e
Juan Saltiva <66804342+juansaltiva2000@users.noreply.github.com>
2025-05-04 23:31:29 -0300

    test login

--- frontend/test/login.spec.js
+++ frontend/test/login.spec.js
@@ -1,19 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('Login exitoso con credenciales correctas', async ({ page }) => {
-  // Navegar a la página de login
   await page.goto('http://localhost:5173/login');
-  
-  // Completar los campos del formulario de login
-  await page.fill('#email', 'admin@test.com'); // Usar un email válido
-  await page.fill('#password', '123456'); // Usar una contraseña válida
-  
-  // Hacer clic en el botón de submit
+  await page.fill('#username', 'cliente@gmail.com');  // Ejemplo de credenciales válidas
+  await page.fill('#password', '1234');
   await page.click('button[type="submit"]');
-  
-  // Verificar que la página de bienvenida o dashboard se cargue
-  await expect(page.locator('text=Bienvenido')).toBeVisible();
-  
-  // Opcionalmente, verificar que la URL haya cambiado a la página de destino (por ejemplo, el dashboard)
-  await expect(page).toHaveURL('http://localhost:5173/dashboard');
+
+  // Verificar que la URL cambió correctamente después de hacer login
+  await expect(page).toHaveURL('http://localhost:5173/employee');  // Cambiar a la URL correcta
 });
---
+++ frontend/test/pedir_licencia.spec.js
@@ -0,0 +1,7 @@
+/////// debemos loguiarlo
+/////// ir  a la pagina de pedir licencia
+/////// llenar el formulario de pedir licencia  
+/////// enviar el formulario
+/////// verificar que se haya enviado correctamente
+//
+import { test, expect } from '@playwright/test';   
