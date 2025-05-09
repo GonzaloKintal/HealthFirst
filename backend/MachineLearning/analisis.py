0d49b6cdcc60a82bff7878f54ed217ded62dec81
FuentesCelina <fuentescelina01@gmail.com>
2025-05-09 01:45:48 -0300

    se suman condicionales para evaluar motivos

--- backend/MachineLearning/analisis.py
+++ backend/MachineLearning/analisis.py
@@ -1,4 +1,4 @@
-
+from datetime import date
 
 #en este archivo se va a realizar el algoritmo que toma las decisiones en cuanto los motivos de liencia
 politicas_tipos = {}
@@ -68,19 +68,45 @@ def license_analysis(licencia):
     tipo = licencia["tipo_licencia"]
     datos = politicas_tipos.get(tipo)
 
+    fecha_actual = date.today()
+
     if not datos:
         return "Este tipo de licencia no se encuentra en la base de datos"
     
+    if licencia["fecha_inicio"]< fecha_actual:
+        return "La fecha del inicio de licencia es anterior a la actual"
+    
+    if licencia["dias_totales"]> get_total_days_res(licencia["dni"]) :
+        return "Los dias solicitados exceden los dias restantes que le quedan al empleado"
 
+    #Limite en pedidos por año
+    if get_res_lim(tipo) >= datos["limite_anual_pedidos"] :
+        return "Se han completado el maximo de pedidos por año"
+    
+    #Dias corridos
+    if get_max_days_cons(tipo) >= datos["max_dias_corridos"] :
+        return "Se han completado el maximo de dias corridos"
+    #Total de dias anual
+    #Minimo de preaviso
+    
     
 
-def get_total_days(empleado): # se obtienen el total de dias para las vacaciones
-    print("")
+def get_total_days_vac(id_empleado): # se obtienen el total de dias para las vacaciones
+    return 0
 
+def get_total_days_res(id_empleado): # se obtienen el total de dias restantes que le quedan por tipo y empleado
+    return 0
+
+def get_res_lim(id_empleado): # se obtienen los pedidos que ya realizó, retorna 0 si no hay limite
+    return 0
+
+def get_max_days_cons(id_empleado): # se obtienen la cantidad de dias corridos que se tomaron
+    return 0
 
 #Las solicitudes que recibiremos seran del tipo
 '''
 solicitud = {
+    "dni":"",
     "tipo_licencia" : "Enfermedad",
     "fecha_inicio": "",
     "fecha_fin": "",
