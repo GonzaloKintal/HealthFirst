8793fa2d834d3917a7ccb6aea8343c6352f26b4a
FuentesCelina <fuentescelina01@gmail.com>
2025-05-08 23:34:24 -0300

    se definen algunas funciones

--- backend/MachineLearning/analisis.py
+++ backend/MachineLearning/analisis.py
@@ -1,77 +1,60 @@
 
 
 #en este archivo se va a realizar el algoritmo que toma las decisiones en cuanto los motivos de liencia
+politicas_tipos = {}
 
 def define_types():
     politicas_tipos= {
         "Vacaciones": {
-            "requiere_justificativo": False,
             "min_preaviso": 2,
-            "tolerancia_justificativo": None,
             "total_dias_anual": None,  #depende de antigüedad, se calcula aparte
             "max_dias_corridos": None,
             "limite_anual_pedidos": 2
         },
         "Nacimiento de hijo" : {
-            "requiere_justificativo": True,
             "min_preaviso": 0,
-            "tolerancia_justificativo": 3,
             "total_dias_anual": None,
             "max_dias_corridos": 2,
             "limite_anual_pedidos": 2
         },
         "Estudios" : {
-            "requiere_justificativo": True,
             "min_preaviso": 3,
-            "tolerancia_justificativo": 6,
             "total_dias_anual": 24,
             "max_dias_corridos": 4,
             "limite_anual_pedidos": None
         },
         "Maternidad" : {
-            "requiere_justificativo": True,
             "min_preaviso": 0,
-            "tolerancia_justificativo": 7,
             "total_dias_anual": 90,
             "max_dias_corridos": None,
             "limite_anual_pedidos": 2
         },
         "Control prenatal" : {
-            "requiere_justificativo": True,
             "min_preaviso": 3,
-            "tolerancia_justificativo": 2,
             "total_dias_anual": None,
             "max_dias_corridos": 1,
             "limite_anual_pedidos": None
         },
         "Accidente de Trabajo" : {
-            "requiere_justificativo": True,
             "min_preaviso": 0,
-            "tolerancia_justificativo": 1,
             "total_dias_anual": None,
             "max_dias_corridos": None,
             "limite_anual_pedidos": None
         },
         "Enfermedad" : {
-            "requiere_justificativo": True,
             "min_preaviso": 0,
-            "tolerancia_justificativo": 1,
             "total_dias_anual": None,
             "max_dias_corridos": None,
             "limite_anual_pedidos": None
         },
         "Casamiento" : {
-            "requiere_justificativo": True,
             "min_preaviso": 7,
-            "tolerancia_justificativo": 7,
             "total_dias_anual": None,
             "max_dias_corridos": 12,
             "limite_anual_pedidos": 1
         },
         "Tramites prematrimoniales" : {
-            "requiere_justificativo": True,
             "min_preaviso": 7,
-            "tolerancia_justificativo": 0,
             "total_dias_anual": None,
             "max_dias_corridos": 1,
             "limite_anual_pedidos": 1
@@ -81,17 +64,29 @@ def define_types():
     }
     print("asd")
 
-def license_analysis():
-    print()
+def license_analysis(licencia):
+    tipo = licencia["tipo_licencia"]
+    datos = politicas_tipos.get(tipo)
 
-#Las solicitudes que recibiremos seran del tipo:
+    if not datos:
+        return "Este tipo de licencia no se encuentra en la base de datos"
+    
+
+    
+
+def get_total_days(empleado): # se obtienen el total de dias para las vacaciones
+    print("")
+
+
+#Las solicitudes que recibiremos seran del tipo
+'''
 solicitud = {
-    "tipo_licencia" : "Enfermedad"
+    "tipo_licencia" : "Enfermedad",
     "fecha_inicio": "",
-    "fecha_fin":.
-    "dias_totales":
+    "fecha_fin": "",
+    "dias_totales":""
 }
-
-#los campos faltates: dias corridos,nro de pedido,dias preaviso debes ser calculados u obtenidos aparte
-
+'''
+#los campos faltates: debes ser calculados u obtenidos aparte
+#la fecha de la licencia debe ser posterior a la fecha actual
 
