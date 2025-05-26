8171cfe359d93acd297d8b9c68595df052f28eea
FuentesCelina <fuentescelina01@gmail.com>
2025-05-26 12:50:04 -0300

    se agrega funcion principal

--- backend/licenses/isolation_forest.py
+++ backend/licenses/isolation_forest.py
@@ -14,7 +14,43 @@ django.setup()
 
 from licenses.models import License
 
-def create_dataframe_supervisor(type):
+
+def create_model_supervisor(path_csv): # le paso el csv para el entreamiento
+    data= pd.read_csv(path_csv)
+    features = data[['total_requests', 'approved_requests', 'rejected_requests', 'approval_rate', 'rejection_rate']]
+
+     # Entrenamiento del modelo Isolation Forest
+    model = IsolationForest(n_estimators=100, contamination=0.2, random_state=42)
+    model.fit(features)
+
+    # Guardar el modelo en un archivo, ESTO ES LO CORRECTO
+    joblib.dump(model, 'isolation_forest_sup_model.pkl') 
+
+    return model # NO deberia retornarlo, pero por ahora para pruebas lo dejo así
+
+
+def anomalies_supervisores(data): #recibe un dataframe
+    print("\n ----------------------ANOMALIAS PARA SUPERVISORES")
+
+    #Cargo el modelo previamente guardado
+    model = joblib.load('isolation_forest_sup_model.pkl')
+    #data= pd.read_csv(path_csv)
+    features = data[['total_requests', 'approved_requests', 'rejected_requests', 'approval_rate', 'rejection_rate']]
+
+
+    data['anomaly_score'] = model.decision_function(features)
+    data['is_anomaly'] = model.predict(features)
+    data['is_anomaly'] = data['is_anomaly'].map({1: 0, -1: 1})  # 1 = Anómalo, 0 = Normal
+    data['approval_rate'] = (data['approval_rate'] * 100).map("{:.2f}%".format)
+    data['rejection_rate'] = (data['rejection_rate'] * 100).map("{:.2f}%".format)
+    #muestro los resultados
+    #print(data[['evaluator_id', 'total_requests', 'approved_requests', 'rejected_requests', 'approval_rate', 'rejection_rate', 'anomaly_score', 'is_anomaly']])
+    print(data.head(10))
+    return(data)
+
+
+
+def create_dataframe_supervisor(type): # esto para lo que pide el admin
     print("El tipo es:",type)
     if(type is None or type==""):
         qs = License.objects.filter(evaluator__isnull=False
@@ -45,36 +81,7 @@ def create_dataframe_supervisor(type):
     #print(df)
     return df
 
-def create_model_supervisor(path_csv):
-    data= pd.read_csv(path_csv)
-    features = data[['total_requests', 'approved_requests', 'rejected_requests', 'approval_rate', 'rejection_rate']]
 
-     # Entrenamiento del modelo Isolation Forest
-    model = IsolationForest(n_estimators=100, contamination=0.2, random_state=42)
-    model.fit(features)
-
-    # Guardar el modelo en un archivo, ESTO ES LO CORRECTO
-    joblib.dump(model, 'isolation_forest_sup_model.pkl') 
-
-    return model # NO deberia retornarlo, pero por ahora para pruebas lo dejo así
-def anomalies_supervisores(data): #por ahora recibe el modelo, pero NO deberia
-    print("\n ----------------------ANOMALIAS PARA SUPERVISORES")
-
-    #Cargo el modelo previamente guardado #ESTO ES LO CORRECTO
-    model = joblib.load('isolation_forest_sup_model.pkl')
-    #data= pd.read_csv(path_csv)
-    features = data[['total_requests', 'approved_requests', 'rejected_requests', 'approval_rate', 'rejection_rate']]
-
-
-    data['anomaly_score'] = model.decision_function(features)
-    data['is_anomaly'] = model.predict(features)
-    data['is_anomaly'] = data['is_anomaly'].map({1: 0, -1: 1})  # 1 = Anómalo, 0 = Normal
-    data['approval_rate'] = (data['approval_rate'] * 100).map("{:.2f}%".format)
-    data['rejection_rate'] = (data['rejection_rate'] * 100).map("{:.2f}%".format)
-    #muestro los resultados
-    #print(data[['evaluator_id', 'total_requests', 'approved_requests', 'rejected_requests', 'approval_rate', 'rejection_rate', 'anomaly_score', 'is_anomaly']])
-    print(data.head(10))
-    return(data)
 
 def generar_csv_supervisores(path_csv='supervisors_data_1000.csv', n=1000, semilla=42):
     np.random.seed(semilla)
@@ -108,7 +115,24 @@ def mostrar_anomalias(cant_filas,dataframe):
     top_anomalies = dataframe.sort_values(by='anomaly_score').head(cant_filas).reset_index(drop=True)
     print(top_anomalies)
 
-def dataframe_pruebas():
+
+
+def obtener_anomalias_supervisores(cant_filas): #FUNCION PRINCIPAL QUE SE USARA EN EL FRONT
+    
+    #el cant_filas es para retornar solo esa cantidad de filas, serian los cant_filas mas anomalos
+    dataframe =  anomalies_supervisores(create_dataframe_supervisor())
+    top_anomalies = dataframe.sort_values(by='anomaly_score').head(cant_filas).reset_index(drop=True)
+    return top_anomalies
+
+    
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------
+#falta evaluar fechas de ingreso, es mas anomalo teniendo en cuenta la fecha en la que el supervisor comenzó a trabajr
+def dataframe_pruebas(): # para pruebas
     data = pd.DataFrame({
     'evaluator_id': [6,7, 8, 9, 10, 11, 12],
     'total_requests': [0,40, 28, 35, 50, 20, 30],
@@ -127,17 +151,17 @@ def dataframe_pruebas():
         axis=1
     )
     return data
-#generar_csv_supervisores()
-#create_model_supervisor('supervisors_data_1000.csv')
-dataframe_anomalias= anomalies_supervisores(dataframe_pruebas())
-mostrar_anomalias(10,dataframe_anomalias)
 
 
 
+def pruebas():
+    dataframe= anomalies_supervisores(dataframe_pruebas())
+    top_anomalies = dataframe.sort_values(by='anomaly_score').head(5).reset_index(drop=True)
+    return top_anomalies
+print(pruebas()) # para pruebas
 
-#---------------------------------------------------------------------------------------------------------------
-#evaluar fechas de ingreso, es mas anomalo teniendo en cuenta la fecha en la que el supervisor comenzó a trabajr
 
+#--proximamente para anomalias en empleados
 def create_dataFrame_empleados():
     #agrupo licencias por empleado
     licencias = License.objects.values('user_id').annotate(
